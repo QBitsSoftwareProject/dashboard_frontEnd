b4831b4995b2c94f1f6896f4b0d46f26ff0a5bbc
darkPixels123 <125457336+darkPixels123@users.noreply.github.com>
2024-04-28 09:58:45 +0530

    final merge

--- project-dashboard/src/pages/resource-management/resource-management.jsx
+++ project-dashboard/src/pages/resource-management/resource-management.jsx
@@ -98,7 +98,6 @@ const ResourceManagement = () => {
     if (file != null) {
       if (file.type.startsWith("video")) {
         if (category === "video") {
-
           const videoResource = {
             title,
             duration,
@@ -192,229 +191,221 @@ const ResourceManagement = () => {
           title: "Oops...",
           text: "Please select a file to upload",
         });
-          const videoRef = ref(storage, `videos/${file.name + v4()}`);
+        const videoRef = ref(storage, `videos/${file.name + v4()}`);
 
-          //video upload
-          uploadBytes(videoRef, file)
-            .then(async () => {
-              console.log(videoResource);
-              const Swal = require("sweetalert2");
-              await axios
-                .post(
-                  "http://localhost:3000/api/v1/resources/video",
-                  videoResource
-                )
-                .then((response) => {
-                  // alert("Video resource uploaded successfully: " + response.data);
-                  Swal.fire({
-                    position: "top-center",
-                    icon: "success",
-                    title: "Video upload successful , Check Resources",
-                    showConfirmButton: false,
-                    timer: 1800,
-                  });
-                  // console.log("Video resource uploaded successfully:", response.data);
-                })
-                .catch((error) => {
-                  // Handle error
-                  Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Error uploading video resource",
-                  });
-                  // console.error("Error uploading resource:", error);
+        //video upload
+        uploadBytes(videoRef, file)
+          .then(async () => {
+            console.log(videoResource);
+            const Swal = require("sweetalert2");
+            await axios
+              .post(
+                "http://localhost:3000/api/v1/resources/video",
+                videoResource
+              )
+              .then((response) => {
+                // alert("Video resource uploaded successfully: " + response.data);
+                Swal.fire({
+                  position: "top-center",
+                  icon: "success",
+                  title: "Video upload successful , Check Resources",
+                  showConfirmButton: false,
+                  timer: 1800,
                 });
-              setTimeout(resetForm, 2000);
-            })
-            .catch(() => {});
-        }
-      } else if (file.type.startsWith("audio")) {
-        if (category === "audio") {
-          const audioResource = {
-            title,
-            duration,
-            tags,
-            ifListen,
-            listenCount,
-            downloadURL,
-          };
-
-          const audioRef = ref(storage, `audios/${file.name + v4()}`);
-
-          //audio upload
-          uploadBytes(audioRef, file)
-            .then(async () => {
-              const Swal = require("sweetalert2");
-              await axios
-                .post("/api/v1/resources/audio", audioResource)
-                .then((response) => {
-                  // Handle success
-                  Swal.fire({
-                    position: "top-center",
-                    icon: "success",
-                    title: "Audio upload successful , Check Resources",
-                    showConfirmButton: false,
-                    timer: 1800,
-                  });
-                  resetForm();
-                })
-                .catch((error) => {
-                  // Handle error
-                  Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Error uploading audio resource",
-                  });
+                // console.log("Video resource uploaded successfully:", response.data);
+              })
+              .catch((error) => {
+                // Handle error
+                Swal.fire({
+                  icon: "error",
+                  title: "Oops...",
+                  text: "Error uploading video resource",
                 });
-              setTimeout(resetForm, 2000);
-            })
-            .catch(() => {});
-        }
-      } else {
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Please select a file to upload",
-        });
+                // console.error("Error uploading resource:", error);
+              });
+            setTimeout(resetForm, 2000);
+          })
+          .catch(() => {});
       }
+    } else if (file.type.startsWith("audio")) {
+      if (category === "audio") {
+        const audioResource = {
+          title,
+          duration,
+          tags,
+          ifListen,
+          listenCount,
+          downloadURL,
+        };
+
+        const audioRef = ref(storage, `audios/${file.name + v4()}`);
+
+        //audio upload
+        uploadBytes(audioRef, file)
+          .then(async () => {
+            const Swal = require("sweetalert2");
+            await axios
+              .post("/api/v1/resources/audio", audioResource)
+              .then((response) => {
+                // Handle success
+                Swal.fire({
+                  position: "top-center",
+                  icon: "success",
+                  title: "Audio upload successful , Check Resources",
+                  showConfirmButton: false,
+                  timer: 1800,
+                });
+                resetForm();
+              })
+              .catch((error) => {
+                // Handle error
+                Swal.fire({
+                  icon: "error",
+                  title: "Oops...",
+                  text: "Error uploading audio resource",
+                });
+              });
+            setTimeout(resetForm, 2000);
+          })
+          .catch(() => {});
+      }
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please select a file to upload",
+      });
     }
   };
 
-    return (
-      <div>
-        <div
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "end",
+        }}
+      >
+        <Dash_btn1 btn_text="VIEW RESOURCES" inlineStyle={styles.btnPosition} />
+      </div>
+      <Grid
+        container
+        rowSpacing={3}
+        style={{ overflowY: "scroll", height: "65vh" }}
+      >
+        <Grid
+          item
+          xs={2}
           style={{
             display: "flex",
-            justifyContent: "end",
+            justifyContent: "start",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ fontWeight: "bold", marginLeft: "20px" }}>
+            Resource name :
+          </span>
+        </Grid>
+        <Grid item xs={10} style={{ paddingRight: "20px" }}>
+          <TextField
+            id="rName"
+            label="Enter resource name"
+            variant="outlined"
+            style={{ width: "100%" }}
+            onChange={handleTitleChange}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          style={{
+            display: "flex",
+            justifyContent: "start",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ fontWeight: "bold", marginLeft: "20px" }}>
+            Resource tags :
+          </span>
+        </Grid>
+        <Grid item xs={10} style={{ paddingRight: "20px" }}>
+          <TextField
+            id="rTags"
+            label="Enter resource tags"
+            variant="outlined"
+            style={{ width: "100%" }}
+            onChange={handleTagsChange}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          style={{
+            display: "flex",
+            justifyContent: "start",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ fontWeight: "bold", marginLeft: "20px" }}>
+            Resource category :
+          </span>
+        </Grid>
+        <Grid item xs={10}>
+          <></>
+          <FormControl style={{ width: "50%" }} size="small">
+            <InputLabel id="demo-select-small-label">
+              Select resource category
+            </InputLabel>
+            <Select
+              labelId="demo-select-large-label"
+              id="rCategory"
+              value={category}
+              label="category"
+              style={{ height: "55px" }}
+              onChange={handleCategoryChange}
+            >
+              <MenuItem value={"video"}>Video</MenuItem>
+              <MenuItem value={"audio"}>Audio</MenuItem>
+              <MenuItem value={"pdf"}>Article(Create article)</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} padding={5}>
+          <DropFileInput
+            isCancelled={isCancel}
+            onFileChange={handleFileChange}
+          />
+        </Grid>
+      </Grid>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "end",
+        }}
+      >
+        <div
+          style={{
+            display:
+              title !== "" && tags !== "" && category !== null && file !== null
+                ? "block"
+                : "none",
           }}
         >
           <Dash_btn1
-            btn_text="VIEW RESOURCES"
+            btn_text="UPLOAD RESOURCE"
             inlineStyle={styles.btnPosition}
+            callFunction={uploadResource}
           />
         </div>
-        <Grid
-          container
-          rowSpacing={3}
-          style={{ overflowY: "scroll", height: "65vh" }}
-        >
-          <Grid
-            item
-            xs={2}
-            style={{
-              display: "flex",
-              justifyContent: "start",
-              alignItems: "center",
-            }}
-          >
-            <span style={{ fontWeight: "bold", marginLeft: "20px" }}>
-              Resource name :
-            </span>
-          </Grid>
-          <Grid item xs={10} style={{ paddingRight: "20px" }}>
-            <TextField
-              id="rName"
-              label="Enter resource name"
-              variant="outlined"
-              style={{ width: "100%" }}
-              onChange={handleTitleChange}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={2}
-            style={{
-              display: "flex",
-              justifyContent: "start",
-              alignItems: "center",
-            }}
-          >
-            <span style={{ fontWeight: "bold", marginLeft: "20px" }}>
-              Resource tags :
-            </span>
-          </Grid>
-          <Grid item xs={10} style={{ paddingRight: "20px" }}>
-            <TextField
-              id="rTags"
-              label="Enter resource tags"
-              variant="outlined"
-              style={{ width: "100%" }}
-              onChange={handleTagsChange}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={2}
-            style={{
-              display: "flex",
-              justifyContent: "start",
-              alignItems: "center",
-            }}
-          >
-            <span style={{ fontWeight: "bold", marginLeft: "20px" }}>
-              Resource category :
-            </span>
-          </Grid>
-          <Grid item xs={10}>
-            <></>
-            <FormControl style={{ width: "50%" }} size="small">
-              <InputLabel id="demo-select-small-label">
-                Select resource category
-              </InputLabel>
-              <Select
-                labelId="demo-select-large-label"
-                id="rCategory"
-                value={category}
-                label="category"
-                style={{ height: "55px" }}
-                onChange={handleCategoryChange}
-              >
-                <MenuItem value={"video"}>Video</MenuItem>
-                <MenuItem value={"audio"}>Audio</MenuItem>
-                <MenuItem value={"pdf"}>Article(Create article)</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} padding={5}>
-            <DropFileInput
-              isCancelled={isCancel}
-              onFileChange={handleFileChange}
-            />
-          </Grid>
-        </Grid>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "end",
-          }}
-        >
-          <div
-            style={{
-              display:
-                title !== "" &&
-                tags !== "" &&
-                category !== null &&
-                file !== null
-                  ? "block"
-                  : "none",
-            }}
-          >
-            <Dash_btn1
-              btn_text="UPLOAD RESOURCE"
-              inlineStyle={styles.btnPosition}
-              callFunction={uploadResource}
-            />
-          </div>
 
-          <Dash_btn2
-            btn_text="CANCEL UPLOAD"
-            inlineStyle={styles.btnPosition}
-            onClickEvent={setIsCancel}
-          />
-        </div>
+        <Dash_btn2
+          btn_text="CANCEL UPLOAD"
+          inlineStyle={styles.btnPosition}
+          onClickEvent={setIsCancel}
+        />
       </div>
-    );
-  };
-;
+    </div>
+  );
+};
 
 export default ResourceManagement;
